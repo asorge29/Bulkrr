0f68b2695998855ddb9e2ea56d9557b98075a7b7
BassMaster629 <oceans4496@gmail.com>
2024-02-29 18:29:27 -0800

    Renaming functionality and statefulness

--- bulkrr/views.py
+++ bulkrr/views.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QWidget, QFileDialog
 from PyQt5 import QtGui
+from PyQt5.QtCore import QThread
 from .ui.window import Ui_Window
+from .rename import Renamer
 from pathlib import Path
 from collections import deque
 
@@ -33,9 +35,34 @@ class Window(QWidget, Ui_Window):
 
     def _setupUI(self):
         self.setupUi(self)
+        self._updateStateWhenNoFiles()
+
+    def _updateStateWhenNoFiles(self):
+        self._filesCount = len(self._files)
+        self.loadFilesButton.setEnabled(True)
+        self.loadFilesButton.setFocus(True)
+        self.renameFilesButton.setEnabled(False)
+        self.prefixEdit.clear()
+        self.prefixEdit.setEnabled(False)
+
+    def _updateStateWhenFilesLoaded(self):
+        self.prefixEdit.setEnabled(True)
+        self.prefixEdit.setFocus(True)
+
+    def _updateStateWhenReady(self):
+        if self.prefixEdit.text():
+            self.renameFilesButton.setEnabled(True)
+        else:
+            self.renameFilesButton.setEnabled(False)
+
+    def _updateStateWhileRenaming(self):
+        self.loadFilesButton.setEnabled(False)
+        self.renameFilesButton.setEnabled(False)
     
     def _connectSignalsSlots(self):
         self.loadFilesButton.clicked.connect(self.loadFiles)
+        self.renameFilesButton.clicked.connect(self.renameFiles)
+        self.prefixEdit.textChanged.connect(self._updateStateWhenReady)
 
     def loadFiles(self):
         self.dstFileList.clear()
@@ -47,6 +74,7 @@ class Window(QWidget, Ui_Window):
             self, "Choose Files to Rename", initDir, filter=FILTERS
         )
         if len(files) > 0:
+            self._updateStateWhenFilesLoaded()
             fileExtension = filter[filter.index("*") : -1]
             self.extensionLabel.setText(fileExtension)
             srcDirName = str(Path(files[0]).parent)
@@ -55,3 +83,37 @@ class Window(QWidget, Ui_Window):
                 self._files.append(Path(file))
                 self.srcFileList.addItem(file)
             self._filesCount = len(self._files)
+    
+    def renameFiles(self):
+        self._runRenamerThread()
+        self._updateStateWhileRenaming()
+
+    def _runRenamerThread(self):
+        prefix = self.prefixEdit.text()
+        self._thread = QThread()
+        self._renamer = Renamer(
+            files=tuple(self._files),
+            prefix=prefix,
+        )
+        self._renamer.moveToThread(self._thread)
+        # Rename
+        self._thread.started.connect(self._renamer.renameFiles)
+        # Update state
+        self._renamer.renamedFile.connect(self._updateStateWhenFileRenamed)
+        self._renamer.progressed.connect(self._updateProgressBar)
+        self._renamer.finished.connect(self._updateStateWhenNoFiles)
+        # Clean up
+        self._renamer.finished.connect(self._thread.quit)
+        self._renamer.finished.connect(self._renamer.deleteLater)
+        self._thread.finished.connect(self._thread.deleteLater)
+        # Run the thread
+        self._thread.start()
+
+    def _updateStateWhenFileRenamed(self, newFile):
+        self._files.popleft()
+        self.srcFileList.takeItem(0)
+        self.dstFileList.addItem(str(newFile))
+
+    def _updateProgressBar(self, fileNumber):
+        progressPercent = int(fileNumber / self._filesCount * 100)
+        self.progressBar.setValue(progressPercent)
